cd6570bc2585684d83f15f0442b8d51224746188
kerimoff <kerimov.nurlan@gmail.com>
2021-06-14 13:59:04 +0300

    remove out of date run scripts

--- run_microarray_qc_only.sh
+++
@@ -1,7 +0,0 @@
-nextflow run quality_control.nf -profile tartu_hpc\
- --is_microarray\
- --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/Naranbhai_2015.tsv.gz\
- --study_name Naranbhai_2015\
- --sample_meta_path /gpfs/hpc/home/kerimov/SampleArcheology/studies/cleaned/Naranbhai_2015.tsv
-
- 
